3f6b38a69e2319142a4da52a8bdddb6935f88602
lindangulopez <angulo.linda@gmail.com>
2026-05-19 18:59:43 +0200

    guix manifest

---
+++ manifest.scm
@@ -0,0 +1,44 @@
+(specifications->manifest
+ (list
+
+  ;; Python
+  "python"
+  "python-pip"
+
+  ;; Jupyter
+  "jupyter"
+
+  ;; Python GIS
+  "python-numpy"
+  "python-pandas"
+  "python-geopandas"
+  "python-rasterio"
+  "python-matplotlib"
+  "python-scipy"
+
+  ;; GIS libs
+  "gdal"
+  "proj"
+  "geos"
+
+  ;; R
+  "r"
+  "r-irkernel"
+  "r-ggplot2"
+  "r-dplyr"
+  "r-tidyr"
+  "r-readr"
+  "r-stringr"
+  "r-purrr"
+  "r-lubridate"
+  "r-sf"
+
+  ;; Julia
+  "julia"
+
+  ;; QGIS
+  "qgis"
+
+  ;; Database
+  "postgresql"
+  "postgis"))
